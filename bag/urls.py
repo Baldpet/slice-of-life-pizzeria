bf0578eaa6590d319c8a88c4ca1d0e2950e2778b
Baldpet <peterdaniel.baldwin@gmail.com>
2021-01-01 13:49:56 +0000

    Updated the view to add sides/drink to the session bag to include qty and price id

--- bag/urls.py
+++ bag/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/addside/<item_id>/', views.add_side_to_bag, name='add_side_to_bag'),
+    path('', views.view_bag, name='view_bag'),
+    path('addside/<item_id>/', views.add_side_to_bag, name='add_side_to_bag'),
+    path('clear_bag/', views.clear_bag, name='clear_bag'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -4,18 +4,35 @@ from products.models import Product
 # Create your views here.
 
 
+def view_bag(request):
+    """ View the current bag contents """
+    bag = request.session.get('bag', {})
+    print(bag)
+    return render(request, 'bag/bag.html')
+
+
+def clear_bag(request):
+    """ Clear the bag of all items """
+
+    del request.session['bag']
+    return render(request, 'bag/bag.html')
+
+
 def add_side_to_bag(request, item_id):
     """ Add a side or a drink to the current bag """
 
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    price = request.POST.get('price')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+        new_quantity = bag[item_id][0]
+        new_quantity += quantity
+        bag[item_id] = new_quantity, price
     else:
-        bag[item_id] = quantity
+        bag[item_id] = quantity, price
 
     request.session['bag'] = bag
 
