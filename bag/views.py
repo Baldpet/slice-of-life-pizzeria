797e451c936b77cf3ae95f987461b4becd272a5a
Baldpet <peterdaniel.baldwin@gmail.com>
2021-01-01 12:06:55 +0000

    Added bag URLs to top level and add side URL

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from products.models import Product
 
 # Create your views here.
+
+
+def add_side_to_bag(request, item_id):
+    """ Add a side or a drink to the current bag """
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+
+    return redirect(redirect_url)
