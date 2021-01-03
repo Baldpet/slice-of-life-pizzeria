f058e0c5da93040fb8b0387ac84c197a6474eaa6
Baldpet <peterdaniel.baldwin@gmail.com>
2021-01-03 16:55:06 +0000

    Added remove item view and urls

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
 from products.models import Product, Price
+from django.contrib import messages
 
 # Create your views here.
 
@@ -29,25 +30,63 @@ def amend_bag(request, item_id):
         original_size = request.POST['original_size']
     bag = request.session.get('bag', {})
 
-    if size:
-        if bag[item_id][dough][size] == 0:
-            bag[item_id][dough][original_size] = 0
-            bag[item_id][dough][size] = quantity
+    try:
+        if size:
+            if bag[item_id][dough][size] == 0:
+                bag[item_id][dough][original_size] = 0
+                bag[item_id][dough][size] = quantity
+            else:
+                bag[item_id][dough][original_size] = 0
+                bag[item_id][dough][size] += quantity
+            if original_size == size:
+                messages.success(request, f'Amended your order of {product.name} - {dough} - {size} to a quantity of {quantity}.')
+            else:
+                messages.success(request, f'Amended your order to {product.name} - {dough} - {size} and a quantity of {quantity}.')
         else:
-            bag[item_id][dough][original_size] = 0
-            bag[item_id][dough][size] += quantity
-    else:
-        bag[item_id] = quantity
+            messages.success(request, f'Amended your order of {product.name} to a quantity of {quantity}.')
+            bag[item_id] = quantity
 
-    request.session['bag'] = bag
+        request.session['bag'] = bag
 
-    return redirect('view_bag')
+        return redirect('view_bag')
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}.')
+        return HttpResponse(status=500)
+
+
+def remove_item_from_bag(request, item_id):
+    """ Remove the selected item from the bag """
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        size = None
+        dough = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+            dough = request.POST['dough']
+        bag = request.session.get('bag', {})
+
+        if size:
+            messages.success(request, f'Removed {product.name} - {dough} - {size} from your order.')
+            bag[item_id][dough][size] = 0
+        else:
+            messages.success(request, f'Removed {product.name} from your order.')
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}.')
+        return HttpResponse(status=500)
 
 
 def clear_bag(request):
     """ Clear the bag of all items """
 
     del request.session['bag']
+    messages.success(request, 'Removed all items from your order.')
     return render(request, 'bag/bag.html')
 
 
@@ -60,30 +99,37 @@ def add_pizza_to_bag(request, item_id):
     dough_base = request.POST.get('dough')
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id][dough_base][size] += quantity
-    else:
-        bag[item_id] = {'classic': {'S': 0,
+    try:
+        if item_id in list(bag.keys()):
+            messages.success(request, f'Added {product.name} - {dough_base} - {size} to your order.')
+            bag[item_id][dough_base][size] += quantity
+        else:
+            bag[item_id] = {'classic': {'S': 0,
+                                        'M': 0,
+                                        'L': 0,
+                                        'XL': 0,
+                                        },
+                            'thin': {'S': 0,
                                     'M': 0,
                                     'L': 0,
                                     'XL': 0,
                                     },
-                        'thin': {'S': 0,
-                                 'M': 0,
-                                 'L': 0,
-                                 'XL': 0,
-                                 },
-                        'stuffed': {'S': 0,
-                                    'M': 0,
-                                    'L': 0,
-                                    'XL': 0,
-                                    },
-                        }
-        bag[item_id][dough_base][size] = quantity
+                            'stuffed': {'S': 0,
+                                        'M': 0,
+                                        'L': 0,
+                                        'XL': 0,
+                                        },
+                            }
+            messages.success(request, f'Added {product.name} - {dough_base} - {size} to your order.')
+            bag[item_id][dough_base][size] = quantity
 
-    request.session['bag'] = bag
+        request.session['bag'] = bag
 
-    return redirect(redirect_url)
+        return redirect(redirect_url)
+
+    except Exception as e:
+        messages.error(request, f'Error adding pizza: {e}.')
+        return HttpResponse(status=500)
 
 
 def add_side_to_bag(request, item_id):
@@ -94,11 +140,18 @@ def add_side_to_bag(request, item_id):
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-    else:
-        bag[item_id] = quantity
+    try:
+        if item_id in list(bag.keys()):
+            messages.success(request, f'Added {product.name} to your order.')
+            bag[item_id] += quantity
+        else:
+            messages.success(request, f'Added {product.name} to your order.')
+            bag[item_id] = quantity
 
-    request.session['bag'] = bag
+        request.session['bag'] = bag
 
-    return redirect(redirect_url)
+        return redirect(redirect_url)
+
+    except Exception as e:
+        messages.error(request, f'Error adding item: {e}.')
+        return HttpResponse(status=500)
