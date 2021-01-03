1864f0466a636ef4c6081f0eee44201e21125564
Baldpet <peterdaniel.baldwin@gmail.com>
2021-01-03 17:29:22 +0000

    Updated the toasts and initialized via base.js

--- static/js/base.js
+++ static/js/base.js
@@ -1,4 +1,5 @@
 var toastElList = [].slice.call(document.querySelectorAll('.toast'))
 var toastList = toastElList.map(function (toastEl) {
-  return new bootstrap.Toast(toastEl, option)
+  return new bootstrap.Toast(toastEl)
 })
+toastList.forEach(toast => toast.show());
