8f1036347fada214bfca7c10860edafe6a11195f
Baldpet <peterdaniel.baldwin@gmail.com>
2021-01-09 19:05:15 +0000

    Added basic cards to the offer view and html

--- offers/views.py
+++ offers/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Offer
+from products.models import Product
 
 # Create your views here.
 
 
 def offers(request):
-    return render(request, 'offers/offers.html')
+    offers = Offer.objects.all()
+    template = 'offers/offers.html'
+    context = {
+        'offers': offers
+    }
+    return render(request, template, context)
