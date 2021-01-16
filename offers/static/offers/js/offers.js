f88808e962295acbe54acc74d4f682c9b5681fd9
Baldpet <peterdaniel.baldwin@gmail.com>
2021-01-16 16:24:01 +0000

    added in ability to select your chosen item and set up the form with the correct hidden inputs

---
+++ offers/static/offers/js/offers.js
@@ -0,0 +1,46 @@
+// Ingredient popover for the pizzas
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
+
+/* To select the item that you want to add to an offer.
+This will outline which item you have selected, unhide those items and then scroll down to them.*/
+
+$('.btn-selector').click(function(event){
+    event.preventDefault()
+    $('.offer-card').removeClass("selected-offer");
+    $(this).parent().parent().toggleClass("selected-offer");
+    let offerItem = $(this).data("offer-item").toLowerCase();
+    $('.product-area').addClass("d-none");
+    $('#' + offerItem).toggleClass("d-none");
+    $("body,html").animate(
+      {
+        scrollTop: $("#" + offerItem).offset().top - 168
+      },
+      400 //speed
+    );
+})
+
+/* To select the respective item and allocate it to the offer item section
+Will then remove the view of other items.
+*/
+
+$('.item-selector').click(function(){
+    let itemID = $(this).attr('id');
+    let itemImage = $(this).data("item-image");
+    let itemNumber = $('.selected-offer').data('item-number');
+    let itemSize = $('.selected-offer').data('item-size');
+    let itemName = $(this).data("item-name");
+    $('.selected-offer').find("img").attr("src", itemImage);
+    $('.selected-offer').find("h5").html(itemName);
+    $('.selected-offer').find("img").removeClass("d-none");
+    $('.selected-offer').find("img").siblings().addClass("d-none");
+    $('.selected-offer').find("select").parent().removeClass("d-none");
+    $('#select-btn-' + itemNumber).addClass("d-none");
+    $('#change-btn-' + itemNumber).removeClass("d-none");
+    $('.offer-card').removeClass("selected-offer");
+    $('.product-area').addClass("d-none");
+    $(`input[name="item${itemNumber}"]`).attr('value', itemID)
+})
+
--- offers/views.py
+++ offers/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Offer
 from products.models import Product
+from products.forms import MiniForm
 
 # Create your views here.
 
@@ -19,11 +20,17 @@ def offers_detail(request, offerId):
     pizzas = Product.objects.all().filter(category__name='Pizza')
     sides = Product.objects.all().filter(category__name='Side')
     drinks = Product.objects.all().filter(category__name='Drink')
+    dough_choices = Product.dough_choices
+    dough_choices_items = []
+    for dough in dough_choices:
+        dough_choices_items.append(dough[0])
+
     template = 'offers/offers_detail.html'
     context = {
         'offer': offer,
         'pizzas': pizzas,
         'sides': sides,
         'drinks': drinks,
+        'dough_choices': dough_choices_items,
     }
     return render(request, template, context)
