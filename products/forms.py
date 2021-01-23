95828aebd3f3eb31ad137f9eba8725050d2ad23e
Baldpet <peterdaniel.baldwin@gmail.com>
2021-01-23 18:40:30 +0000

    Formatting the add a product form with Form Helper

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from .models import Product
 
+from crispy_forms.helper import FormHelper, Layout
+from crispy_forms.layout import Fieldset, Div, HTML, ButtonHolder, Submit
+
 
 class MiniForm(forms.Form):
     dough_choices = Product.dough_choices
@@ -14,3 +17,63 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.template_pack = 'bootstrap4'
+        self.helper.form_method = 'post'
+        self.helper.form_action = 'add_product'
+        self.helper.layout = Layout(
+            Fieldset( '',
+                Div('name', css_class="m-3"),
+                Div('category', css_class="m-3"),
+                Div('dough', css_class="m-3"),
+                Div('sauce', css_class="m-3"),
+                Div('cheese', css_class="m-3"),
+                Div(
+                    HTML("<h4>Additional Toppings</h4>"),
+                    css_class="row m-3 text-center"
+                ),
+                Div(
+                    Div('chicken', css_class="col-6 col-md-3"),
+                    Div('pepperoni', css_class="col-6 col-md-3"),
+                    Div('bacon', css_class="col-6 col-md-3"),
+                    Div('sausage', css_class="col-6 col-md-3"),
+                    css_class="row mt-3 m-md-3"
+                ),
+                Div(
+                    Div('ham', css_class="col-6 col-md-3"),
+                    Div('meatball', css_class="col-6 col-md-3"),
+                    Div('chorizo', css_class="col-6 col-md-3"),
+                    Div('mushroom', css_class="col-6 col-md-3"),
+                    css_class="row m-md-3"
+                ),
+                Div(
+                    Div('pepper', css_class="col-6 col-md-3"),
+                    Div('onion', css_class="col-6 col-md-3"),
+                    Div('chilli', css_class="col-6 col-md-3"),
+                    Div('pineapple', css_class="col-6 col-md-3"),
+                    css_class="row m-md-3"
+                ),
+                Div(
+                    Div('key_lime', css_class="col-6 col-md-3"),
+                    Div('extra_cheese', css_class="col-6 col-md-3"),
+                    css_class="row mb-3 m-md-3"
+                ),
+                Div('description', css_class="m-3"),
+                Div(
+                    HTML("<h4>Price</h4>"),
+                    css_class="row m-3 text-center"
+                ),
+                Div('is_premium', css_class="m-3"),
+                Div(
+                    HTML("<p>Product Price:</p>"),
+                    css_class="product_price"
+                ),
+                Div('image', css_class="m-3"),
+            ),
+            ButtonHolder(
+                Submit('submit', 'Add Product', css_class='btn btn-primary m-3')
+            )
+        )
