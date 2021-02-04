701097eb94afd3f0f8fd26a5d998d772e87b406d
Baldpet <peterdaniel.baldwin@gmail.com>
2021-02-04 16:13:58 +0000

    Added checkout success view and adjusted the form to submit order and redirect

--- checkout/views.py
+++ checkout/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.conf import settings
+from django.contrib import messages
 
 from .forms import OrderForm
+from .models import Order, OrderLineItem
 from profiles.models import UserProfile
 
 from bag.context import bag_contents
 
 import stripe
+import json
 
 # Create your views here.
 
@@ -14,6 +17,33 @@ import stripe
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+    if request.method == 'POST':
+        bag = request.session.get('bag', {})
+
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_adress2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.loyalty_points = 0
+            order.save()
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form.')
 
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
@@ -46,6 +76,20 @@ def checkout(request):
         'form': form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret
+    }
+
+    return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    order = get_object_or_404(Order, order_number=order_number)
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order
     }
 
     return render(request, template, context)
