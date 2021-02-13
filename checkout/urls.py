86c7ada8bebd7931990c134a118950ff957433f0
Baldpet <peterdaniel.baldwin@gmail.com>
2021-02-13 19:39:27 +0000

    adjusted the checkout js to post extra meta data to use on webhooks

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
     path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
     path('pizza_tracker/<order_number>', views.pizza_tracker, name='pizza_tracker'),
     path('update_delivery_or_collection/', views.update_delivery_or_collection, name='update_delivery_or_collection'),
--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse, HttpResponse, Http404
+from django.views.decorators.http import require_POST
 from django.conf import settings
 from django.contrib import messages
 
@@ -14,6 +15,23 @@ import stripe
 import json
 
 # Create your views here.
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'username': request.user,
+            'save_info': request.POST.get('save-info'),
+            'bag': json.dumps(request.session.get('bag', {})),
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
