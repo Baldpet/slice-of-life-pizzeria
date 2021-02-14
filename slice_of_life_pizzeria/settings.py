2e1a9a54d1ad06e7c07d1a6a787e9b3e3507940a
Baldpet <peterdaniel.baldwin@gmail.com>
2021-02-14 18:31:22 +0000

    added allowed hosts and database element to settings

--- slice_of_life_pizzeria/settings.py
+++ slice_of_life_pizzeria/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://baldpet-slice-of-life.herokuapp.com/', 'localhost']
 
 
 # Application definition
@@ -129,12 +129,17 @@ WSGI_APPLICATION = 'slice_of_life_pizzeria.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
