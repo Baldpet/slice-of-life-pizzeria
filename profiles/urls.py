a5df603c780f33b953a4b76d8988e5729d62a4c8
Baldpet <peterdaniel.baldwin@gmail.com>
2021-03-08 17:18:55 +0000

    Amend & delete offer/product added to views and urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('product_management', views.product_management, name='product_management'),
+    path('product_amend/<product_id>', views.amend_product, name='amend_product'),
+    path('offer_amend/<offer_id>', views.amend_offer, name='amend_offer'),
+    path('product_delete/<product_id>', views.delete_product, name='delete_product'),
+    path('offer_delete/<offer_id>', views.delete_offer, name='delete_offer'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from .forms import UserForm
 from .models import UserProfile
+
+from offers.models import Offer
+from offers.forms import OfferForm
+from products.models import Product
+from products.forms import ProductForm
 
 # Create your views here.
 
@@ -28,9 +33,50 @@ def profile(request):
 
 
 def product_management(request):
+    offers = Offer.objects.all()
+    products = Product.objects.all().order_by('category')
+
     template = 'profiles/product_management.html'
     context = {
-
+        'offers': offers,
+        'products': products,
     }
 
     return render(request, template, context)
+
+
+def amend_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    form = ProductForm(instance=product)
+
+    template = 'profiles/amend_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+def amend_offer(request, offer_id):
+    offer = get_object_or_404(Offer, pk=offer_id)
+    form = OfferForm(instance=offer)
+    template = 'profiles/amend_offer.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product successfully deleted.')
+    return redirect('product_management')
+
+
+def delete_offer(request, offer_id):
+    offer = get_object_or_404(Offer, pk=offer_id)
+    offer.delete()
+    messages.success(request, 'Offer successfully deleted.')
+    return redirect('product_management')
