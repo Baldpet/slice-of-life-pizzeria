d04942d70f2fffc47d6f3e5571031b7d95ba4ebe
Baldpet <peterdaniel.baldwin@gmail.com>
2021-03-09 14:55:21 +0000

    Error made on migration of images - have corrected

--- offers/models.py
+++ offers/models.py
@@ -33,7 +33,7 @@ class Offer(models.Model):
     original_price = models.DecimalField(max_digits=6, decimal_places=2, default=Decimal(0))
     deal_price = models.DecimalField(max_digits=6, decimal_places=2)
     saving = models.DecimalField(max_digits=6, decimal_places=2, default=Decimal(0))
-    image = models.ImageField(upload_to='images/', null=False, blank=False)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
 
     def __str__(self):
         return self.name
